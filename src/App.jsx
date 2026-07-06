6877d7bb5bb15e069fa9b41a63dde668229f62ed
ekancisme <lethecuong2k4@gmail.com>
2026-07-06 23:26:57 +0700

    feat: Add Essay History page with status filter, search and pagination

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,7 @@ import SetWeeklyGoals from './pages/student/SetWeeklyGoals'
 import AIFeedbackReview from './pages/student/AIFeedbackReview'
 import VocabularyLibrary from './pages/student/VocabularyLibrary'
 import FlashcardReview from './pages/student/FlashcardReview'
+import EssayHistory from './pages/student/EssayHistory'
 
 /* Teacher Pages */
 import TeacherDashboard from './pages/teacher/TeacherDashboard'
@@ -47,6 +48,7 @@ function App() {
         <Route path="write-essay" element={<WriteEssay />} />
         <Route path="vocabulary" element={<VocabularyLibrary />} />
         <Route path="vocabulary/review" element={<FlashcardReview />} />
+        <Route path="essays" element={<EssayHistory />} />
         <Route path="progress" element={<MyProgress />} />
         <Route path="goals" element={<SetWeeklyGoals />} />
         <Route path="feedback" element={<AIFeedbackReview />} />
